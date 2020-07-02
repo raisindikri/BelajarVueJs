0b1808b301f2b9530a090f21225eb72e56b20c5a
raisusmanadzikri <raisusmanadzikri@gmail.com>
2020-07-02 16:57:16 +0700

    mencoba dulu

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    show: false,
+    message: '<i><b>Hello WOrd</b></i>',
+    message2: 'Hello vue',
+    int1: 1,
+    int2: 2,
+    result: null,
+    kilometers: 0,
+    meters: 0,
+  },
+  computed: {
+    sum: function () {
+      return this.int1 + this.int2;
+    }
+  },
+  methods: {
+    sumProcess: function (int3) {
+      return this.result = this.int1 + this.int2 + int3;
+    }
+  },
+  watch: {
+    kilometers: function (val) {
+      this.kilometers = val;
+      this.meters = val * 1000;
+
+    },
+    meters: function (val) {
+      this.kilometers = val / 1000;
+      this.meters = val;
+    }
+
+  }
+})
